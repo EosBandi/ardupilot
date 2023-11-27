f87487ff44e62983fb0bd070004ae167722d6b8c
EosBandi <eosbandi@gmail.com>
2023-11-28 00:25:58 +0100

    Version change

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.4.3-LUABackport"
+#define THISFIRMWARE "ArduCopter V4.4.3-LB-SO"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,4,3,FIRMWARE_VERSION_TYPE_OFFICIAL
